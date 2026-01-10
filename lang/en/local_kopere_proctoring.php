143235b466e5005c02f1d3ee86a2aab37c095537
Eduardo Kraus <kraus@eduardokraus.com>
2026-01-10 06:08:27 -0300

    moodle.Files.LangFilesOrdering.IncorrectOrder

--- lang/en/local_kopere_proctoring.php
+++ lang/en/local_kopere_proctoring.php
@@ -15,6 +15,8 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
+ * phpcs:disable moodle.Files.LangFilesOrdering.IncorrectOrder
+ *
  * En lang file
  *
  * @package   local_kopere_proctoring
