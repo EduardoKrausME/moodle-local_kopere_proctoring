9579bafb0078b56711bf9f1846dc7529027ea7e0
Eduardo Kraus <kraus@eduardokraus.com>
2026-06-29 17:39:39 -0300

    Expected MOODLE_INTERNAL check or config.php inclusion

--- policy/password/pending.php
+++ policy/password/pending.php
@@ -15,6 +15,8 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
+ * phpcs:disable moodle.Files.MoodleInternal.MoodleInternalGlobalState
+ *
  * Lightweight pending-password checker.
  *
  * This endpoint intentionally does not require a logged-in user. It reads only
